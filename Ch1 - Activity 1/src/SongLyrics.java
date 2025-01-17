a19e0eb70f7f4b4b98424d1a333cc088d31e34ad
Big-Bear07 <larahtripp@gmail.com>
2025-01-17 09:44:28 -0500

    Add files via upload

---
+++ Ch1 - Activity 1/src/SongLyrics.java
@@ -0,0 +1,14 @@
+// Tripp 1/14 - Ch.1 Activity 1
+public class SongLyrics {
+
+	public static void main(String[] args) {
+		System.out.println("So here we see it's such an ordinary night");
+		System.out.println("The city lights, the noises, and the you I find.");
+		System.out.println("Again, the clock's face is all frozen in time.");
+		System.out.println("The cluttered room stays as a place to drop on by.");
+		System.out.println("While nothing else changed, looking back I came to find");
+		System.out.println("We've been blind and gotten older all this time.");
+		
+	}
+
+}
---
+++ Ch1 - Activity 2/src/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+// Tripp 1/14 - Ch.1 Activity 2
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		System.out.println("I feel the need - the need for speed!");
+		System.out.println("Peter 'Maverick' Mitchell");
+		System.out.println("Top Gun");
+		System.out.println("Year: 1986");
+
+	}
+
+}
